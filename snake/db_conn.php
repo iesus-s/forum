3ce33b55f52cf378a6038bda3efb15e20766bd93
Jesus Sandoval <sandoval.j4@outlook.com>
2024-12-13 10:04:14 -0800

    nginx but broken

--- snake/db_conn.php
+++ snake/db_conn.php
@@ -1,7 +1,7 @@
 <?php
     $sname = "mysql";
     $uname = "root";
-    $password = "password";
+    $password = "32-117-kill3rb33";
     $db_name = "the_database";
 
     $conn = mysqli_connect($sname, $uname, $password, $db_name); 
